fb5839045e01642f31b52cc866c9ad25d3e6cd35
ohn582 <49073115+ohn582@users.noreply.github.com>
2020-01-21 15:44:00 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
 
   get '/eletronics', to: "eletronics#index"
   get '/eletronics/:id', to: "eletronics#show"
-  # post '/eletronics', to: "eletronics#create"
 end
