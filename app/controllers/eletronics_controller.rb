5e04814f2bdbd320cb896607a39b158f51c62633
ohn582 <49073115+ohn582@users.noreply.github.com>
2020-01-22 15:36:29 -0500

    Update eletronics_controller.rb

--- app/controllers/eletronics_controller.rb
+++ app/controllers/eletronics_controller.rb
@@ -5,13 +5,6 @@ class EletronicsController < ApplicationController
         render json: @eletronic
     end
 
-    def create
-        @eletronic = Eletronic.create(eletronic_params)
-        if @eletronic.valid?
-            render json: @eletronic
-        end
-    end
-
     # rendering a eletronic
     def show
         render json: Eletronic.find(params[:id])
